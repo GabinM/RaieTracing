b544a0cfba934fc8270d669c9d950cd18599db18
GabinM <gabin.mathieu54@gmail.com>
2025-05-27 16:45:47 +0200

    c'est mieux avec les bons imports

--- NoeudCalcul.java
+++ NoeudCalcul.java
@@ -1,6 +1,10 @@
+import raytracer.Scene;
+import raytracer.Image;
+import java.rmi.RemoteException;
+
 public class NoeudCalcul implements ServiceNoeudCalcul {
 
-    public Image computeImage(scene, int x, int y, int w, int l) throws RemoteException{
+    public Image computeImage(Scene scene, int x, int y, int w, int l) throws RemoteException {
         return null;
     }
 
--- ServiceNoeudCalcul.java
+++ ServiceNoeudCalcul.java
@@ -1,7 +1,8 @@
-import java.rmi.Remote;
 import java.rmi.RemoteException;
+import raytracer.Scene;
+import raytracer.Image;
 
 public interface ServiceNoeudCalcul {
 
-    Image computeImage(scene, int x, int y, int w, int l) throws RemoteException;
+    Image computeImage(Scene scene, int x, int y, int w, int l) throws RemoteException;
 }
